f8c9ab31b2a00952e2eaf4b817bae7f88df2be83
bigdumpy <massivedumpy@protonmail.com>
2021-08-06 14:42:05 +0300

    Change chance for enc to 100% for testing

--- src/RandomEnchants.cpp
+++ src/RandomEnchants.cpp
@@ -44,17 +44,17 @@ public:
 
 		int slotRand[3] = { -1, -1, -1 };
 		uint32 slotEnch[3] = { 0, 1, 5 };
-		double roll1 = rand_chance();
+		double roll1 = 70.0; // rand_chance()
 		if (roll1 >= 70.0)
 			slotRand[0] = getRandEnchantment(item);
 		if (slotRand[0] != -1)
 		{
-			double roll2 = rand_chance();
+			double roll2 = 65.0; // rand_chance()
 			if (roll2 >= 65.0)
 				slotRand[1] = getRandEnchantment(item);
 			if (slotRand[1] != -1)
 			{
-				double roll3 = rand_chance();
+				double roll3 = 65.0; // rand_chance()
 				if (roll3 >= 60.0)
 					slotRand[2] = getRandEnchantment(item);
 			}
